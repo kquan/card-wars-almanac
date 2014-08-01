890998ae6c3cb545abf026b325c91f9c235f1a8b
Kevin Quan <kevin@samsungaccelerator.com>
2014-08-01 09:11:48 -0400

    Fix typo

--- java/src/com/kevinquan/cwa/model/spells/WizardRawk.java
+++ java/src/com/kevinquan/cwa/model/spells/WizardRawk.java
@@ -21,7 +21,7 @@ public class WizardRawk extends AbstractSpellCard {
     private static final String TAG = WizardRawk.class.getSimpleName();
     
     public WizardRawk() {
-        super("WizardRawk", 3, Rarity.VeryRare);
+        super("Wizard Rawk", 3, Rarity.VeryRare);
         mDescription = "Choose one of your creatures and give it Attack equal to how much Damage it has taken"; // Check
     }
 
